639808680ab9364b55e63d27ad619629dedec2fc
adrienclesse <adrienclesse@hotmail.com>
2021-01-07 10:59:15 +0100

    Last version currency

--- index.php
+++ index.php
@@ -3,44 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./css/style.css">
     <style type="text/css"></style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <title>The-Dream-Currency</title>
 </head>
 <body>
+
+<?php
+ini_set('display_errors','1');
+if(isset($_POST['convert'])){
+$amount=$_POST['amount'];
+$country=$_POST['country'];
+
+if($country=="EUR"){
+    $result=$amount*0.15."€";
+    $currency= "€";
+}
+
+if($country=="AUD"){
+    $result=$amount*0.24."AU$";
+    $currency= "AU$";
+}
+if($country=="USD"){
+    $result=$amount*0.19."$";
+    $currency= "$";
+}
+}
+
+
+?>
     <div class="converting">
         <h1>Brazil : A useful converter </h1>
-        <h4>Convert the price of your cocktail and avoid getting ripped off</h4>
-            <p> Brasilian Reais :</p>
-            <form  method="post">
+        
+        <p> Brasilian Real :</p>
+        <form  method="post">
+        
             <div class="input-group">
-                <input type="number" class="form-control" name="amount">
-   <div class="input-group-append">
-       <span class="input-group-text"> R$ </span>
-   </div>   
-</div>
-<br>
-<button id=convert name="convert">Convert</button>
-<br><br>
-    <p>Euros :</p>
-    <div class="input-group">
-   <input type="number" class="form-control" name="result">
-   <div class="input-group-append">
-       <span class="input-group-text"> € </span>
-
-   </div>
-   </form>
-</div>
-    
+            <input type="number" step="0.01" class="input" placeholder="0.00" name="amount">
+                <div class="input-group-append">
+                    <span class="input-group-text"> R$ </span>
+                </div>   
+            </div>
+            <br>
+            <button id=convert name="convert">Convert</button>
+            <br><br>
+            <select id="country" name="country">
+            <option name='USD' value="USD">USA</option>
+            <option name='EUR' value="EUR">EURO</option>
+            <option name='AUD' value="AUD">Australia</option>
+        
+         </select>
+         <br><br>
+            <div class="input-group">
+            <input type="number" step="0.01" class="input"  name="euros" placeholder=<?php echo $result ?> >
+                <div class="input-group-append">
+                    <span class="input-group-text"> <?php echo $currency ?>  </span>
+                </div>
+            </div>
+        </form>
     </div>
+    
+    
 
-<?php 
-//if(isset($_POST['convert'])){
-//$amount = $_POST['amount'];
-//echo $amount*2
-//}
-?>
+
 
 </body>
 </html>
